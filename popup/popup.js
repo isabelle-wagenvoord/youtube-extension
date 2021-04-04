a59d2579f590fd7728ba4f4aba447950f98f2f44
isabelle.wagenvoord <isabellewagenvoord@gmail.com>
2021-04-03 23:31:49 -0700

    Cleaning up UI part 1

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+// https://developer.chrome.com/docs/extensions/mv3/manifest/web_accessible_resources/
+// "web_accessible_resources": [
+// 		{
+// 			"resources": ["frame.html"],
+// 			"matches": ["chat-url-here"]
+// 		}
+//     ],
